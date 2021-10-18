087c731c7019382f51ee84a7321df22a6a7a7eee
Vladimir <Vladimir@macbook-pro.home>
2021-10-19 01:32:50 +0200

    NavBar for mobiles. New Crypto Api query getExchanges. Fetch exchanges info. Exchanges page.

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+
+import { Link } from "react-router-dom";
+import { Card, Row, Col } from "antd";
+import millify from "millify";
+
+import { useGetExchangesQuery } from "../services/cryptoApi";
 
 const Exchanges = () => {
+    const { data, isFetching } = useGetExchangesQuery();
+    const exchangesList = data?.data?.exchanges;
+
+    if (isFetching) return "Loading";
     return (
-        <div>
-            
-        </div>
+        <>
+           <Row gutter={[32, 32]} className="crypto-card-container">
+        {exchangesList.map((exchange) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={exchange.id}>
+            <a href={exchange.websiteUrl}>
+              <Card
+                title={`${exchange.rank}. ${exchange.name}`}
+                extra={<img className="crypto-image" src={exchange.iconUrl} />}
+                hoverable
+              >
+                <p>24h Volume: $ {millify(exchange.volume)}</p>
+                <p>Markets: {millify(exchange.numberOfMarkets)}</p>
+              </Card>
+            </a>
+          </Col>
+        ))}
+      </Row> 
+        </>
     )
 }
 
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -21,10 +21,13 @@ export const cryptoApi = createApi({
         getCryptoHistory: builder.query({
             query: ({coinId, timePeriod}) => createRequest(`/coin/${coinId}/history/${timePeriod}`)
         }),
+        getExchanges: builder.query({
+            query: () => createRequest(`/exchanges`)
+        }),
     })
 });
 
 export const {
-    useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery
+    useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery
 } = cryptoApi;
   
